96c14c05e1d6ba4d18d165dc571c1fc378d8e5f4
anant-bahuguna <anantbahugunaddn@gmail.com>
2019-06-12 13:53:31 +0530

    first commit

---
+++ 1-basics/main.js
@@ -0,0 +1,114 @@
+// console.log(3+1)
+
+// var age = 50;
+
+
+// if(age>65)
+// {
+// 	console.log('senior')
+// }
+// else
+// {
+// 	console.log('junior')
+// }
+
+
+
+function  getArea(l,b)
+{
+	return l*b
+}
+
+// console.log(getArea(4,5))
+
+// alert(getArea(4,5))
+
+// var name = prompt('enter name')
+// console.log(name)
+
+var a=3;
+var b=4;
+var c=5;
+
+// if(a>=b && a>=c)
+// 	console.log(a)
+// else if(b>=a && b>=c)
+// 	console.log(b)
+// else
+// 	console.log(c)
+
+// if(a>b)
+// {
+// 	if(a>c)
+// 		console.log(a)
+// 	else
+// 		console.log(c)
+// }
+// else
+// {
+// 	if(b>c)
+// 		console.log(b)
+// 	else
+// 		console.log(c)
+// }
+
+
+// var max = a;
+// if(b>max)
+// 	max=b;
+// if(c>max)
+// 	max=c;
+// console.log(max)
+
+
+
+var list = [1, 2, 3, 4, 5];
+
+// console.log(list[list.length-1])
+
+
+// LOOPS
+
+
+// for (var i=0; i<5; i++) {
+// 	console.log(i)
+
+// }
+
+
+// for (var k in list) {
+// 	console.log(list[k])
+// }
+
+
+// JSON
+
+var person = {
+	name: 'Anant',
+	age: 21,
+	address: {
+		city: 'Delhi',
+		zip: 110001,
+	},
+	friends: [1,2,3]
+}
+
+// console.log(person['name'])
+
+// console.log(person.name)
+
+var field = 'address';
+console.log(person[field]['zip'])
+
+
+
+//EcmaScript
+
+
+
+
+
+
+
+
+
